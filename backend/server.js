03afbc4d3e2708c0656e3d5cc173715315a40fcd
Shreyansh <root@localhost.localdomain>
2019-06-02 15:13:46 +0530

    Adding configuration changes

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const mongodb = require('mongodb');
+
 const app = express();
+const mongoClient = mongodb.MongoClient;
+const config = require('./configuration.json');
 
 const staticPath = path.join( __dirname, 'ParkingLot');
 const indexHTML = path.join( staticPath, 'index.html');
+const ENV = config.envToRun;
+const PORT = config[ENV].port;
 
-console.log( indexHTML );
-console.log( staticPath );
+// console.log( indexHTML );
+// console.log( staticPath );
+// console.log( config[ENV] );
+// console.log( mongoClient );
 
 app.use( express.static( staticPath ));
 
@@ -17,7 +25,14 @@ app.get('*', (req,res) => {
     fs.createReadStream( indexHTML ).pipe(res);
 });
 
+mongoClient.connect( config[ENV].mongoUri,  {useNewUrlParser: true }, (err, db) =>{
+    console.log( 'connected to mongoDB' );
+    db.db('parkingLotProd').listCollections().toArray(function(err, collInfos) {
+        console.log( collInfos );
+    });
+});
 
-app.listen(3000, () => {
-    console.log('Server started and listening at 3000');
+
+app.listen(PORT, () => {
+    console.log(`Server started and listening at ${PORT}`);
 });
